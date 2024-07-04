006712a685a3c4c7ca5ccffa0d0da1e43f682bf7
chae04 <cotnqls04@gmail.com>
2024-07-04 14:53:23 +0900

    2024-07-04 K번째 수

---
+++ 채수빈/정렬/K번째수.py
@@ -0,0 +1,18 @@
+# commands = [i, j, k]
+
+
+def solution(array, commands):
+    answer = []
+    
+    for i in range(len(commands)):
+        arr = []    # 자른 배열 담기
+        for j in range(commands[i][0], commands[i][1]+1):
+            arr.append(array[j-1])
+        arr.sort()
+        answer.append(arr[commands[i][2]-1])
+    
+    return answer
+
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(array, commands))
